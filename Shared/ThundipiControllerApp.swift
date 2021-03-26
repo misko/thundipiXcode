b3fd31b67291ec24079cdf4630cfdaf3661e6173
misko <misko@cs.toronto.edu>
2021-03-26 15:45:21 -0700

    Initial Commit

---
+++ Shared/ThundipiControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThundipiControllerApp.swift
+//  Shared
+//
+//  Created by Michael Dzamba on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct ThundipiControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
